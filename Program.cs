a9fab79eb653c0d15e338f0e2d44149e8cc43272
fabriciosuhet <fabriciossuhet@gmail.com>
2024-09-30 18:05:06 -0300

    criando minimal api dotnet

---
+++ Program.cs
@@ -0,0 +1,33 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+
+app.MapPost("/login", (LoginDTO loginDTO) => {
+    if (loginDTO.Email == "adm@teste.com" && loginDTO.Senha == "123456") 
+        return Results.Ok("Login com sucesso");
+    else
+        return Results.Unauthorized();
+});
+
+app.Run();
+
+public class LoginDTO
+{
+    public string Email { get; set; } = default!;
+    public string Senha { get; set; } = default!;
+}
